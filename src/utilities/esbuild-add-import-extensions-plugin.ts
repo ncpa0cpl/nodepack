0dad304a8b27a52310edb20c87683bf8c040677e
ncpa0cpl <szymonb21@gmail.com>
2022-08-25 21:30:38 +0200

    fix: corret import rewriting for directories imports

--- src/utilities/esbuild-add-import-extensions-plugin.ts
+++ src/utilities/esbuild-add-import-extensions-plugin.ts
@@ -1,13 +1,33 @@
 import type esbuild from "esbuild";
+import type { Stats } from "fs";
+import fs from "fs/promises";
+import path from "path";
 
 export const ESbuildAddImportExtensionsPlugin = (ext: string) => ({
-  name: "esbuild-esm-import-plugin",
+  name: "nodepack-esbuild-add-import-extensions-plugin",
   setup(build: esbuild.PluginBuild) {
-    build.onResolve({ filter: /.*/ }, (args) => {
+    build.onResolve({ filter: /.*/ }, async (args) => {
       if (args.importer) {
-        if (args.path.startsWith("."))
-          return { path: args.path + ext, external: true };
-        else
+        if (args.path.startsWith(".")) {
+          let stat: Stats | undefined = undefined;
+
+          try {
+            stat = await fs.stat(path.resolve(args.resolveDir, args.path));
+          } catch (e) {
+            //
+          }
+
+          if (stat?.isDirectory()) {
+            let p = args.path;
+            if (p.endsWith("/")) {
+              p = p.slice(0, -1);
+            }
+            return {
+              path: `${p}/index${ext}`,
+              external: true,
+            };
+          } else return { path: `${args.path}${ext}`, external: true };
+        } else
           return {
             path: args.path,
             external: true,
