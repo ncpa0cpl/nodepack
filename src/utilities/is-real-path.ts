16b01b65d0024595adef1c32f8347ce5f0ea4657
ncpa0cpl <sbretner@touchbistro.com>
2022-09-08 14:35:20 +0200

    feat: better import ext detection

---
+++ src/utilities/is-real-path.ts
@@ -0,0 +1,21 @@
+import fsc from "fs";
+import fs from "fs/promises";
+import { CacheMap } from "./info-cache";
+
+const resultCache = new CacheMap<boolean>();
+
+export const isRealPath = async (path: string) => {
+  const cachedResult = resultCache.get(path);
+  if (cachedResult !== undefined) {
+    return cachedResult;
+  }
+
+  try {
+    await fs.access(path, fsc.constants.R_OK);
+    resultCache.set(path, true);
+    return true;
+  } catch {
+    resultCache.set(path, false);
+    return false;
+  }
+};
--- test/src/index.ts
+++ test/src/index.ts
@@ -1,3 +1,4 @@
+import { Model } from "@Root/common/db.model";
 import { test as T } from "@Root/common/test";
 import data from "./base64.data";
 import file from "./file.json";
@@ -6,6 +7,7 @@ import test from "./test.custom";
 import text from "./text-file.txt";
 
 export {
+  Model,
   file,
   text,
   data,
